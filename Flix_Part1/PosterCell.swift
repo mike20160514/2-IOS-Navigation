efc52bd3aa7d4e23ee8b16e69702f383226c74f2
mike20160514 <mikecao2014@gmail.com>
2018-12-11 02:28:12 -0500

    upload project

---
+++ Flix_Part1/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  Flix_Part1
+//
+//  Created by Mike Su on 10/23/18.
+//  Copyright © 2018 Mike Su. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ Flix_Part1/Views/MovieCell.swift
@@ -0,0 +1,31 @@
+//
+//  MovieCell.swift
+//  Flix_Part1
+//
+//  Created by Mike Su on 10/2/18.
+//  Copyright © 2018 Mike Su. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
